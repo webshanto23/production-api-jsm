4f7b3430e3d6873734d68236737d8946a27d36ca
shanto0241 <shantonu2050@gmail.com>
2026-01-21 19:59:53 +0600

    feat:added lint prettier format and fix check all

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("Hello from the Express.js server!");
+app.get('/', (req, res) => {
+  res.send('Hello from the Express.js server!');
 });
 
 export default app;
