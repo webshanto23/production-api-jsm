df94079dfdbcdaeb04330f014b1642ecd5732842
shanto0241 <shantonu2050@gmail.com>
2026-01-21 19:20:57 +0600

    feat:express server created

--- index.js
+++
@@ -1,1 +0,0 @@
-console.log("This is the production API for JSM.");
---
+++ src/app.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello from the Express.js server!");
+});
+
+export default app;
